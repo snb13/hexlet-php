8ad487b5b5c02a746f1b0af244e141fa53f2302f
a.maksimov <snb13@bk.ru>
2025-07-22 01:30:24 +0300

    updated composer.json and other files added

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
 
-echo 'Hello, Hexlet!';
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
